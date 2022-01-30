c459753480b53749d4cc571074d21dbb761b590b
Akash Verma <65110494+shawnfrost69@users.noreply.github.com>
2022-01-31 02:50:05 +0530

    Add files via upload

--- SEARCHING AND SORTING/SelectionSort.java
+++ SEARCHING AND SORTING/SelectionSort.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class SelectionSort {
     public static void selectionSort(int[]arr){
-
         for (int i = 0; i < arr.length-1; i++) {
             int min = i;
             for (int j = i+1; j < arr.length; j++) {
