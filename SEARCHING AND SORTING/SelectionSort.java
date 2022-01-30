e4a3e4f7ba77dbebd9ac7028b2fff65de3dc9097
shawnfrost69 <liteyagami.69@gmail.com>
2022-01-31 01:12:22 +0530

    Selection Sort

---
+++ SEARCHING AND SORTING/SelectionSort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class SelectionSort {
+    public static void selectionSort(int[]arr){
+
+        for (int i = 0; i < arr.length-1; i++) {
+            int min = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min]  = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        selectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
+    }
+}
